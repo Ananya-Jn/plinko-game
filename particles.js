ce09ebe7ee8747eb3ed186c830dc990854ce707d
Ananya-Jn <75167161+Ananya-Jn@users.noreply.github.com>
2021-07-08 10:21:58 +0530

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,29 @@
+class Particle{
+    constructor(x, y, r) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        
+        this.y= y;
+        this.x =x;
+        this.r =r
+        this.body = Bodies.circle(x, y, r, options);
+        this.color=(random(0,255),random(0,255),random(0,255) )
+        World.add(world, this.body);
+      }
+      display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        fill(this.color)
+        ellipseMode(RADIUS)
+        ellipse(0,0,this.r,this.r)
+        pop();
+      }
+}
+
+
